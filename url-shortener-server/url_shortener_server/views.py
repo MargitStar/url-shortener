0548bae07569b9778449854a8e0e35913e0933e6
Margarita Babkova <marharyta.babkova@itechart-group.com>
2022-04-14 12:48:09 +0300

    Add endpoint to redirect

--- url-shortener-server/url_shortener_server/views.py
+++ url-shortener-server/url_shortener_server/views.py
@@ -1,6 +1,4 @@
-from re import S
-
-from flask import jsonify, request
+from flask import jsonify, redirect, request
 from flask.views import MethodView
 from url_shortener_db.models import URL
 from url_shortener_server.serializers import URLLongSchema, URLShortSchema
@@ -16,12 +14,22 @@ class URLLongView(MethodView):
 
 class URLShortView(MethodView):
     def get(self, short_url):
-        result = URL.get_by_short_url(short_url=short_url)
-        return jsonify(URLLongSchema().dump(result)), 200
+        long_url = URL.get_by_short_url(short_url=short_url)
+        return jsonify(URLLongSchema().dump(long_url)), 200
+
+
+class URLRedirectView(MethodView):
+    def get(self, short_url):
+        long_url = URL.get_by_short_url(short_url=short_url)
+        return redirect(URLLongSchema().dump(long_url)["long_url"]), 301
 
 
 app.add_url_rule("/api/long-urls/", view_func=URLLongView.as_view("long-urls"))
 
 app.add_url_rule(
     "/api/short-urls/<string:short_url>/", view_func=URLShortView.as_view("short-urls")
+)
+
+app.add_url_rule(
+    "/<string:short_url>/", view_func=URLRedirectView.as_view("redirect-urls")
 )
