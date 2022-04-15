d1cc049d9c2448b162b6561614fc576e8b774e43
Margarita Babkova <marharyta.babkova@itechart-group.com>
2022-04-16 00:27:28 +0300

    Add CORS

--- url-shortener-server/url_shortener_server/app.py
+++ url-shortener-server/url_shortener_server/app.py
@@ -1,9 +1,11 @@
 import logging
 
 from flask import Flask
+from flask_cors import CORS
 from url_shortener_server.base_routers import initialize_routes
 
 app = Flask(__name__)
+CORS(app)
 
 app.logger.setLevel(logging.INFO)
 initialize_routes(app)
